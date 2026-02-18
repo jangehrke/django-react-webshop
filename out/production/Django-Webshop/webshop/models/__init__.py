9688a8e3713eee404d3fa452d68307f0a96545d7
JanCG <jancg@outlook.de>
2026-02-18 01:55:02 +0100

    Remove out folder from repo

--- out/production/Django-Webshop/webshop/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import User
-from .product import Product
-from .address import Address
-from .order import Order, OrderItem
-from .cart import Cart, CartItem
-from .category import Category
-
-
-__all__ = ["User", "Product", "Address", "Order", "OrderItem", "Cart", "CartItem", "Category"]
